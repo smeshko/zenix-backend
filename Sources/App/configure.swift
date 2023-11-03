2f3059cdf3390a69f415076e7a94a82393792879
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2023-11-03 17:08:38 +0200

    Configure updates

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,22 +6,12 @@ import FluentPostgresDriver
 import Prometheus
 import Metrics
 
-extension Application {
-//    static let databaseUrl = URL(string: Environment.get("DB_URL")!)!
-}
-
 public func configure(_ app: Application) throws {
 
-//    try app.databases.use(.postgres(url: Application.databaseUrl), as: .psql)
     if let databaseURL = Environment.get("DATABASE_URL") {
         try app.databases.use(.postgres(url: databaseURL), as: .psql)
     } else {
         app.logger.error("DATABASE_URL empty")
-        // Handle missing DATABASE_URL here...
-        //
-        // Alternatively, you could also set a different config
-        // depending on wether app.environment is set to to
-        // `.development` or `.production`
     }
 
     let promClient = PrometheusMetricsFactory(client: PrometheusClient())
